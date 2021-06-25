6fdc0198c1399214068067007d8d11cb510bb7f5
Juan Fuentes <43227783+Juanandresfl@users.noreply.github.com>
2021-06-25 11:03:18 -0500

    .

--- shopping-service/src/main/java/academy/digitallab/store/shopping/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/ShoppingServiceApplication.java
@@ -14,7 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableHystrixDashboard
 public class ShoppingServiceApplication {
 
-	pub lic static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(ShoppingServiceApplication.class, args);
 	}
 
